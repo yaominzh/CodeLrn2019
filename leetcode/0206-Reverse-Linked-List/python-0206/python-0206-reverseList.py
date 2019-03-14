580d6ced5cc9ab479ea225a6eaa4c71ca892ea2e
Yaomin Zhang <mcuallen@gmail.com>
2019-03-15 06:39:33 +0800

    0206

---
+++ leetcode/0206-Reverse-Linked-List/python-0206/python-0206-reverseList.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        cur=head
+        prev=None
+        while cur:
+            cur.next, cur, prev = prev, cur.next, cur
+        return prev
