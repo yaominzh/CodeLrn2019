dff92f53e68338a0ec0eaab36dee5fe80ee5375a
Yaomin Zhang <mcuallen@gmail.com>
2019-03-27 20:52:45 +0800

    play with algorithms- 插入排序

---
+++ mooc-Play-with-Algorithms/chap02/InsertionSort/SortTestHelper.h
@@ -0,0 +1,57 @@
+//
+// Created by zhangym on 2019-03-26.
+//
+
+#ifndef SELECTIONSORT_SORTTESTHELPER_H
+#define SELECTIONSORT_SORTTESTHELPER_H
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+namespace SortTestHelper {
+    //生成有n个元素的随机数据，每个元素的随机范围为[rangeL, rangeR]
+    int *generateRandomArray(int n, int rangeL, int rangeR) {
+        assert(rangeL <= rangeR);
+        int *arr = new int[n];
+        srand(time(NULL));//随机种子生成函数
+        for (int i = 0; i < n; i++)
+            arr[i] = rand() % (rangeR - rangeL);
+        return arr;
+    }
+
+    template<typename T>
+    void printArray(T arr[], int n) {
+
+        for (int i = 0; i < n; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+        return;
+    }
+
+    template<typename T>
+    bool isSorted(T arr[], int n) {
+        for (int i = 0; i < n - 1; i++)
+            if (arr[i] > arr[i + 1])
+                return false;
+        return true;
+    }
+
+    template<typename T>
+    void testSort(string sortName, void(*sort)(T[], int), T arr[], int n) {
+        clock_t startTime = clock();
+        sort(arr, n);
+        clock_t endTime = clock();
+        assert(isSorted(arr, n));
+        cout << sortName << "  :  " << double(endTime - startTime) / CLOCKS_PER_SEC << " s" << endl;
+        return;
+    }
+
+    int *copyIntArray(int a[], int n) {
+        int *arr = new int[n];
+        copy(a, a + n, arr);
+        return arr;
+    }
+
+}
+#endif //SELECTIONSORT_SORTTESTHELPER_H
---
+++ mooc-Play-with-Algorithms/chap02/InsertionSort/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include "SortTestHelper.h"
+#include "SelectionSort.h"
+
+using namespace std;
+
+template<typename T>
+void insertionSort(T arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        for (int j = i; j > 0; j--) {
+            if (arr[j] < arr[j - 1])
+                swap(arr[j], arr[j - 1]);
+            else
+                break;
+        }
+    }
+}
+
+int main() {
+    int n = 10000;
+    int *arr = SortTestHelper::generateRandomArray(n, 0, n);
+    int *arr2 = SortTestHelper::copyIntArray(arr, n);
+    SortTestHelper::testSort("Insertion Sort", insertionSort, arr, n);
+    SortTestHelper::testSort("Selection Sort", selectionSort, arr2, n);
+
+
+    delete[] arr;
+    delete[] arr2;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
