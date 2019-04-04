ade616edb91168ffad3688b6b4afb930879acdf4
YaoMinZhang <mcuallen@gmail.com>
2019-04-04 15:14:54 +0800

    Flink入门及实战chap02

---
+++ FlinkExample/src/main/java/az/ee/BatchWordCountJava.java
@@ -0,0 +1,37 @@
+package az.ee;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.util.Collector;
+
+public class BatchWordCountJava {
+    public static void main(String[] args) throws Exception {
+        String inputPath = "/home/allenz/data/file";
+        String outPath = "/home/allenz/data/result";
+
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        DataSource<String> text = env.readTextFile(inputPath);
+        DataSet<Tuple2<String, Integer>> counts = text.flatMap(new Tokenizer()).groupBy(0).sum(1);
+        counts.writeAsCsv(outPath, "\n", " ", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
+        env.execute("batch word count");
+
+    }
+
+    public static class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+//            \\正则表达
+            String[] tokens = value.toLowerCase().split("\\W+");
+            for (String token : tokens) {
+                if (token.length() > 0) {
+                    out.collect(new Tuple2<String, Integer>(token, 1));
+                }
+            }
+        }
+
+    }
+
+}
