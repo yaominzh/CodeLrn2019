fe131252060c0c56ba52e0ddb579758e4c699d4b
yaominzhang <mcuallen@qq.com>
2019-04-18 09:13:04 +0800

    6-3

---
+++ mooc-Play-with-Data-Structure/src/chap06BST/BST.java
@@ -0,0 +1,57 @@
+package chap06BST;
+
+public class BST<E extends Comparable<E>> {
+    private class Node {
+        public E e;
+        public Node left, right;
+
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public BST() {
+        root = null;
+        size = 0;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(E e) {
+        if (root == null) {
+            root = new Node(e);
+        } else
+            add(root, e);
+    }
+
+    private void add(Node node, E e) {
+        if (node.e.equals(e)) {
+            return;
+        } else if (e.compareTo(node.e) < 0 && node.left == null) {
+            node.left = new Node(e);
+            size++;
+            return;
+        } else if (e.compareTo(node.e) > 0 && node.right == null) {
+            node.right= new Node(e);
+            size++;
+            return;
+        }
+        if(e.compareTo(node.e)<0)
+            add(node.left,e);
+        else
+            add(node.right,e);
+
+    }
+
+}
