699e321b03d35a1cfc6d52194b25ffb0cc959ee1
yaominzh <mcuallen@gmail.com>
2019-11-30 16:37:04 +0800

    mooc43 chap07 readgraph test

--- mooc43-bobo-algo/sanbox/chap07/Graph/SparseGraph.h
+++ mooc43-bobo-algo/sanbox/chap07/Graph/SparseGraph.h
@@ -1,9 +1,5 @@
-//
-// Created by Allen Zhang on 2019-11-30.
-//
-
-#ifndef GRAPH_SPARCEGRAPH_H
-#define GRAPH_SPARCEGRAPH_H
+#ifndef GRAPH_SPARSEGRAPH_H
+#define GRAPH_SPARSEGRAPH_H
 
 #include <iostream>
 #include <vector>
@@ -11,72 +7,103 @@
 
 using namespace std;
 
-class SparseGraph {
+// 稀疏图 - 邻接表
+class SparseGraph{
+
 private:
-    int n, m;
-    bool directed;
-    vector<vector<int>> g;
+    int n, m;       // 节点数和边数
+    bool directed;  // 是否为有向图
+    vector<vector<int>> g;  // 图的具体数据
+
 public:
-    SparseGraph(int n, bool directed) {
+    // 构造函数
+    SparseGraph( int n , bool directed ){
+        assert( n >= 0 );
         this->n = n;
-        this->m = 0;
+        this->m = 0;    // 初始化没有任何边
         this->directed = directed;
-        for (int i = 0; i < n; i++)
-            g.push_back(vector<int>());
+        // g初始化为n个空的vector, 表示每一个g[i]都为空, 即没有任和边
+        g = vector<vector<int>>(n, vector<int>());
     }
 
-    ~SparseGraph() {
+    ~SparseGraph(){ }
 
-    }
+    int V(){ return n;} // 返回节点个数
+    int E(){ return m;} // 返回边的个数
 
-    int V() { return n; }
+    // 向图中添加一个边
+    void addEdge( int v, int w ){
 
-    int E() { return m; }
+        assert( v >= 0 && v < n );
+        assert( w >= 0 && w < n );
 
-    void addEdge(int v, int w) {
-        assert(v >= 0 && v < n);
-        assert(v >= 0 && w < n);
         g[v].push_back(w);
-        if (v != w && !directed)
+        if( v != w && !directed )
             g[w].push_back(v);
-        m++;
+
+        m ++;
     }
 
-    bool hasEdge(int v, int w) {
-        assert(v >= 0 && v < n);
-        assert(w >= 0 && w < n);
-        for (int i = 0; i < g[v].size(); i++)
-            if (g[v][i] == w)
+    // 验证图中是否有从v到w的边
+    bool hasEdge( int v , int w ){
+
+        assert( v >= 0 && v < n );
+        assert( w >= 0 && w < n );
+
+        for( int i = 0 ; i < g[v].size() ; i ++ )
+            if( g[v][i] == w )
                 return true;
         return false;
     }
 
-    class adjIterator {
+    // 显示图的信息
+    void show(){
+
+        for( int i = 0 ; i < n ; i ++ ){
+            cout<<"vertex "<<i<<":\t";
+            for( int j = 0 ; j < g[i].size() ; j ++ )
+                cout<<g[i][j]<<"\t";
+            cout<<endl;
+        }
+    }
+
+    // 邻边迭代器, 传入一个图和一个顶点,
+    // 迭代在这个图中和这个顶点向连的所有顶点
+    class adjIterator{
     private:
-        SparseGraph &G;
+        SparseGraph &G; // 图G的引用
         int v;
         int index;
+
     public:
-        adjIterator(SparseGraph &graph, int v) : G(graph) {
-            this->v =v;
+        // 构造函数
+        adjIterator(SparseGraph &graph, int v): G(graph){
+            this->v = v;
             this->index = 0;
         }
 
-        int begin() {
+        ~adjIterator(){}
+
+        // 返回图G中与顶点v相连接的第一个顶点
+        int begin(){
             index = 0;
-            if (G.g[v].size())
+            if( G.g[v].size() )
                 return G.g[v][index];
+            // 若没有顶点和v相连接, 则返回-1
             return -1;
         }
 
-        int next() {
-            index++;
-            if (index < G.g[v].size())
+        // 返回图G中与顶点v相连接的下一个顶点
+        int next(){
+            index ++;
+            if( index < G.g[v].size() )
                 return G.g[v][index];
+            // 若没有顶点和v相连接, 则返回-1
             return -1;
         }
 
-        bool end() {
+        // 查看是否已经迭代完了图G中与顶点v相连接的所有顶点
+        bool end(){
             return index >= G.g[v].size();
         }
     };
--- mooc43-bobo-algo/sanbox/chap07/Graph/main.cpp
+++ mooc43-bobo-algo/sanbox/chap07/Graph/main.cpp
@@ -1,43 +1,12 @@
 #include <iostream>
 #include <vector>
 #include "DenseGraph.h"
-#include "DenseGraph.h"
+#include "SparseGraph.h"
 #include "ReadGraph.h"
 
 int main() {
-    int N = 20;
-    int M = 100;
-    srand(time(NULL));
-    cout << rand();
-    cout << endl;
-    DenseGraph g1(N, false);
-    for (int i = 0; i < M; i++) {
-        int a = rand() % N;
-        int b = rand() % N;
-        g1.addEdge(a, b);
-    }
-    for(int v=0;v<N;v++){
-        cout<<v<<" : ";
-        DenseGraph::adjIterator adj(g1, v);
-        for(int w = adj.begin();!adj.end();w=adj.next())
-            cout<<w<<" ";
-        cout<<endl;
-    }
-    cout<<endl;
-
-    DenseGraph g2(N, false);
-    for (int i = 0; i < M; i++) {
-        int a = rand() % N;
-        int b = rand() % N;
-        g1.addEdge(a, b);
-    }
-    for(int v=0;v<N;v++){
-        cout<<v<<" : ";
-        DenseGraph::adjIterator adj(g1, v);
-        for(int w = adj.begin();!adj.end();w=adj.next())
-            cout<<w<<" ";
-        cout<<endl;
-    }
-    cout<<endl;
-    return 0 ;
+    string filename = "../testG1.txt";
+    SparseGraph g1(13, false);
+    ReadGraph<SparseGraph> readGraph(g1, filename);
+    g1.show();
 }
